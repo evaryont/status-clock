1ca397f2c4733661724b0aa7164f21857e73e669
Colin Shea <colin@evaryont.me>
2013-07-21 15:48:19 -0700

    Convert the Users controller & model to strong_parameters
    The controller is supposed to whitelist the parameters, not the model, in Rails
    4+. Also, extract the find call to a private method (similar to Rails scaffolds)
    for a bit of DRY cleanup.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,32 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
   before_filter :correct_user?, :except => [:index]
+  before_filter :find_user, :only => [:edit, :update, :show]
 
   def index
     @users = User.all
   end
 
-    def edit
-    @user = User.find(params[:id])
+  def edit
   end
   
   def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(params[:user])
+    if @user.update_attributes(user_params)
       redirect_to @user
     else
       render :edit
     end
   end
 
-
-def show
-    @user = User.find(params[:id])
+  def show
   end
 
+  private
+    def find_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:provider, :uid, :name, :email)
+    end
 end
