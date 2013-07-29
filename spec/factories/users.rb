af95179a9987ca418236ba8bcef9aed941cad1fb
Colin Shea <colin@evaryont.me>
2013-07-29 12:42:57 -0700

    Match factory to current schema

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,17 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :user do
-    provider "twitter"
-    uid "12345"
-    name "Bob"
+    provider "google_oauth2"
+    uid "116582821578363186745"
+    name "Tom"
+
+    status_1 'Work'
+    status_2 'Home'
+    status_3 'Play'
+    status_4 'Mall'
+    status_5 'Sleep'
+    status_6 'School'
+
+    token_code 'ab12.XYZ3CS4YjRPU4oMGC3M66uvYEZ8YfqxkrZ0BGtTc8e3wMl4'
+    expires_at Time.now + 1.week
   end
 end
