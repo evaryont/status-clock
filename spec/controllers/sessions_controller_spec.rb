bf5a598569268ee96353b7d0eca48d7ecc16f168
Colin Shea <colin@evaryont.me>
2013-07-30 20:14:31 -0700

    Convert the mocked omniauth to Google/"Tom"
    The default omniauth specs test against Twitter and a user called Bob. Replacing
    it with information about Google and a user called Tom.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 
 describe SessionsController do
-
   before(:each) do
     OmniAuth.config.test_mode = true
-    OmniAuth.config.mock_auth[:twitter] = {
-        'uid' => '12345',
-        'provider' => 'twitter',
-        'info' => {
-          'name' => 'Bob'
-        }
+    OmniAuth.config.mock_auth[:google_oauth2] = {
+       "provider" => "google_oauth2",
+       "uid" => "116582821578363186745",
+       "info" => {
+            "name" => "Tom",
+            "urls" => {"Google"=>"https://plus.google.com/116582821578363186745"}
+       }
       }
   end
 
   describe "GET 'new'" do
     it "redirectes users to authentication" do
       get 'new'
-      assert_redirected_to '/auth/twitter'
+      assert_redirected_to '/auth/google_oauth2'
     end
   end
 
@@ -24,14 +24,15 @@ describe SessionsController do
     it "redirects new users with blank email to fill in their email" do
       @user = FactoryGirl.create(:user)
       visit '/signin'
-      page.should have_content('Logged in as Bob')
+      page.should have_content('Logged in as Tom')
       page.should have_content('Please enter your email address')
       current_path.should == edit_user_path(@user)
     end
+
     it "redirects users with email back to root_url" do
-      @user = FactoryGirl.create(:user, :email => "Tester@testing.com")
+      @user = FactoryGirl.create(:user, :email => "test@gmail.com")
       visit '/signin'
-      page.should have_content('Signed in!')
+      page.should have_content('Logout')
       current_path.should == '/'
     end
   end
