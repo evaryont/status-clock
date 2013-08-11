1bcf2b22945ce2b30f5d60fcda4d7a9914da1adb
Colin Shea <colin@evaryont.me>
2013-08-10 21:48:16 -0700

    Clock.init_with_statuses creates a clock with statuses
    This creates a clock, a number of statuses (the user chooses, defaults to 6),
    and associates those statuses with the clock that was juts made. Then associates
    the user itself with the clock.

--- app/models/clock.rb
+++ app/models/clock.rb
@@ -1,4 +1,22 @@
 class Clock < ActiveRecord::Base
-  has_many :user
-  has_many :status
+  has_many :users
+  has_many :statuses
+
+  # Create a new clock, along with a number of statuses. These new statuses will
+  # serve to be the initial statuses displayed on the clock.
+  def self.init_with_statuses(from_user, lcd_count = 6)
+    new_clock = Clock.new
+    new_clock.statuses = (0..(lcd_count-1)).map do |lcd_position|
+      default_status = Status.new
+      default_status.text = Status::DEFAULTS[lcd_position]
+      default_status.lcd = lcd_position
+      default_status.save!
+      default_status
+    end
+    new_clock.users = [from_user]
+    new_clock.save!
+
+    from_user.clock = new_clock
+    from_user.save!
+  end
 end
--- app/models/status.rb
+++ app/models/status.rb
@@ -1,5 +1,8 @@
 class Status < ActiveRecord::Base
+  DEFAULTS = %w[Home Work School Sleep Games Shopping Flying]
+
   belongs_to :user
+  belongs_to :clock
 
   def to_s
     if lcd.nil?
