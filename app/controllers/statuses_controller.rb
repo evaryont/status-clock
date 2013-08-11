c4135147b8685ef67f833c998e9a35ea04f5c691
Colin Shea <colin@evaryont.me>
2013-08-11 00:09:46 -0700

    After updating, a status redirects to it's clock

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -42,7 +42,7 @@ class StatusesController < ApplicationController
   def update
     respond_to do |format|
       if @status.update(status_params)
-        format.html { redirect_to @status, notice: 'Status was successfully updated.' }
+        format.html { redirect_to @status.clock, notice: 'Status was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
