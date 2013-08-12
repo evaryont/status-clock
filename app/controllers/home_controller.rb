6008963f2e56e658ee87026c36f617b7c695fd97
Colin Shea <colin@evaryont.me>
2013-08-12 10:14:19 -0700

    Only show google information when the user is logged in
    The google_* methods require access to the user's refresh & access tokens, which
    of course are missing when they aren't logged in. (As the application can't
    identify them, yada yada.)
    
    Also, when showing profile information, include their avatar, for giggles.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,15 @@
 class HomeController < ApplicationController
   def index
-    @users = User.all
-    @profile = google_api_call({
-      api_method: google_plus.people.get,
-      parameters: { 'userId' => 'me' }
-    }).data
+    if current_user
+      @profile = google_api_call({
+        api_method: google_plus.people.get,
+        parameters: { 'userId' => 'me' }
+      }).data
 
-    @latest_post = google_api_call({
-      api_method: google_plus.activities.list,
-      parameters: { 'userId' => 'me', 'collection' => 'public' }
-    }).data.items.first
+      @latest_post = google_api_call({
+        api_method: google_plus.activities.list,
+        parameters: { 'userId' => 'me', 'collection' => 'public' }
+      }).data.items.first
+    end
   end
 end
