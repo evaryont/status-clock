26c9e5a0c533ffc4a85e0b444040187ad6b38d24
Colin Shea <colin@evaryont.me>
2013-08-14 11:18:25 -0700

    The user has to choose an account and approve
    See https://github.com/zquestz/omniauth-google-oauth2/issues/66 for more
    information, but both is necessary to get Google to provide me with a
    refresh_token.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET'], {
     scope: 'plus.me,userinfo.email,userinfo.profile',
     access_type: 'offline',
-    prompt: 'select_account'
+    prompt: 'select_account consent'
   }
 end
