9ae3711bfef41d303f9e67325700e6fcb0ce0cef
Colin Shea <colin@evaryont.me>
2013-08-14 14:41:19 -0700

    Don't set container-based variables if there isn't a container
    In Firefox & Chrome, various elements of the website would break unless they
    were on the clock page, because they would stop processing a lot of things once
    a javascript error happened. Fix the error, fix the various issues cropping up
    from uninitialized things. In particular, the two issues I was having: the
    status menu would not drop-down and the Leaflet map would not initialize.

--- app/assets/javascripts/clocks.js
+++ app/assets/javascripts/clocks.js
@@ -2,10 +2,13 @@
 function distributeFields() {
     var fields = $('.status'), container = $('#status-container'),
         width = container.width(), height = container.height(),
-        angle = Math.PI+Math.PI/2, step = (2*Math.PI) / fields.length,
-        basex = container.position().left + container.data('offsetx'),
-        basey = container.position().top + container.data('offsety'),
-        radius = container.data('radius');
+        angle = Math.PI+Math.PI/2, step = (2*Math.PI) / fields.length;
+
+    if (container.position() !== undefined) {
+        var basex = container.position().left + container.data('offsetx'),
+            basey = container.position().top + container.data('offsety'),
+            radius = container.data('radius');
+    }
 
     fields.each(function() {
         var x = Math.round(width/2 + radius * Math.cos(angle) - $(this).width()/2 + basex);
